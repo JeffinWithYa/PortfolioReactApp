1a5744a55ac5f6b4b90ab91426b58891e6e88e60
Jeffrey Jeyachandren <jjeyachandren@blackberry.com>
2020-12-01 00:04:09 -0500

    Updates

---
+++ precache-manifest.fac3eab923bb1f5fe420729a17cb07bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83de86a67bf3d1f84028",
+    "url": "/me/static/css/main.88bec1b2.chunk.css"
+  },
+  {
+    "revision": "83de86a67bf3d1f84028",
+    "url": "/me/static/js/main.83de86a6.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9e4046c85ea54de8741c31e38c6c4460",
+    "url": "/me/index.html"
+  }
+];
