f0958911a880e45d94f76c99fe003b35d8f2dcd2
Jeffrey Jeyachandren <jjeyachandren@blackberry.com>
2023-07-01 09:09:08 -0400

    Updates

---
+++ precache-manifest.b653e79bd07942ca1091e71894053ab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e0c1511fcff75c0e771",
+    "url": "/PortfolioReactApp/static/css/main.2ead1d8a.chunk.css"
+  },
+  {
+    "revision": "3e0c1511fcff75c0e771",
+    "url": "/PortfolioReactApp/static/js/main.3e0c1511.chunk.js"
+  },
+  {
+    "revision": "610bbd81c17326feef18",
+    "url": "/PortfolioReactApp/static/js/runtime~main.610bbd81.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/PortfolioReactApp/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "4e3bcffd0b518c2b965c667a6707ed64",
+    "url": "/PortfolioReactApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.d48c02d3bc7172a5d4daf99221ebb4cc.js"
+  "/PortfolioReactApp/precache-manifest.b653e79bd07942ca1091e71894053ab0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me/index.html", {
+workbox.routing.registerNavigationRoute("/PortfolioReactApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
