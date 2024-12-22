4018a0319bc0751d06a36b73c8103e877e743930
JeffinWithYa <jjeyachandren@gmail.com>
2024-12-21 21:58:09 -0500

    update sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,10 +8,10 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
+              <div className="author-img" style={{ backgroundImage: 'url(images/about.jpg)' }} />
               <h1 id="colorlib-logo"><a href="index.html">Jeffrey Jeyachandren</a></h1>
-              
-              <span className="email">CISSP, Master's Cybersecurity (grad. Apr '25)</span>
+
+              <span className="email">CISSP, Master's Cybersecurity (Expected Apr 2025)</span>
               <p>    </p>
               <p>    </p>
             </div>
@@ -28,7 +28,7 @@ export default class Sidebar extends Component {
               </div>
             </nav>
             <nav id="colorlib-main-menu">
-            <p>    </p>
+              <p>    </p>
               <ul>
                 <li><a href="https://www.linkedin.com/in/jeffreyjeyachandren/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/JeffinWithYa" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
@@ -37,7 +37,7 @@ export default class Sidebar extends Component {
             </nav>
             <div className="colorlib-footer">
               <p><small>
-                  <i className="icon-heart" aria-hidden="true" /> <i className="icon-coffee" aria-hidden="true"></i><br></br>
+                <i className="icon-heart" aria-hidden="true" /> <i className="icon-coffee" aria-hidden="true"></i><br></br>
               </small></p>
             </div>
           </aside>
